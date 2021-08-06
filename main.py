6f46f162299e3a38d166dcc3b841ea7892a40ba0
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2021-08-07 00:23:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 
 
-class Ui_MainWindow(object):
+class Ui_Registrasi(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_Registrasi()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
